363dbba980da15228de7b3931e067d66a7b2ef8c
Hector Piñero <hpineroo@gmail.com>
2017-12-15 19:30:07 -0300

    Estilo y estructura para agregar entradas al kardex
    Agregado los estilos para la mejor visualizacion de las datas!

--- src/app/kardex/kardex/kardex.component.ts
+++ src/app/kardex/kardex/kardex.component.ts
@@ -14,6 +14,8 @@ export class KardexComponent implements OnInit {
   listadoPersonas: any;
   listadoProveedores: any;
   listadoProductos: any;
+  seleccionadoEntrada: boolean = false;
+  seleccionadoSalida: boolean = false;
 
   model: any = {};
 
@@ -21,6 +23,8 @@ export class KardexComponent implements OnInit {
     this.listadoPersonas = this.personas.mostrar();
     this.listadoProductos = this.productos.mostrar();
     this.listadoProveedores = this.proveedores.mostrar();
+    let today = new Date();
+    this.model.fecha = today.getFullYear() + '-' + ('0' + (today.getMonth() + 1)).slice(-2) + '-' + ('0' + today.getDate()).slice(-2);
    }
 
   ngOnInit() {
@@ -28,6 +32,16 @@ export class KardexComponent implements OnInit {
 
   agregarEntrada(){
     console.log(this.model);
+  }
+
+  seleccionarEntrada(){
+    this.seleccionadoEntrada = true;
+    this.seleccionadoSalida = false;
+  }
+
+  seleccionarSalida(){
+    this.seleccionadoEntrada = false;
+    this.seleccionadoSalida = true;
   }
 
 }
