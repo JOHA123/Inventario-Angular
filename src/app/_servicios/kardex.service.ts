9226eed88579358c096051b18773e49d2044130d
Hector Piñero <hpineroo@gmail.com>
2017-12-18 18:36:41 -0300

    Agregar Entrada / Salida

--- src/app/_servicios/kardex.service.ts
+++ src/app/_servicios/kardex.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class KardexService {
 
-  constructor() { }
+  kardex: any;
+
+  constructor(public firebase: AngularFireDatabase) {
+    this.kardex = this.firebase.list('kardex');
+   }
+
+   agregar(kardex){
+    this.kardex.push(kardex);
+   }
+
+   mostrar(){
+    return this.kardex.valueChanges();
+   }
 
 }
--- src/app/kardex/kardex/kardex.component.ts
+++ src/app/kardex/kardex/kardex.component.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs/Observable';
 import { PersonasService } from 'app/_Servicios/personas.service';
 import { ProveedoresService } from 'app/_Servicios/proveedores.service';
 import { ProductosService } from 'app/_Servicios/productos.service';
+import { KardexService } from 'app/_Servicios/kardex.service';
 
 @Component({
   selector: 'app-kardex',
@@ -14,15 +15,19 @@ export class KardexComponent implements OnInit {
   listadoPersonas: any;
   listadoProveedores: any;
   listadoProductos: any;
+  listadoKardex: any;
+  
   seleccionadoEntrada: boolean = false;
   seleccionadoSalida: boolean = false;
+  
 
   model: any = {};
 
-  constructor(public personas: PersonasService, public proveedores: ProveedoresService, public productos: ProductosService) {
+  constructor(public personas: PersonasService, public proveedores: ProveedoresService, public productos: ProductosService, public kardex: KardexService) {
     this.listadoPersonas = this.personas.mostrar();
     this.listadoProductos = this.productos.mostrar();
     this.listadoProveedores = this.proveedores.mostrar();
+    this.listadoKardex = this.kardex.mostrar();
     let today = new Date();
     this.model.fecha = today.getFullYear() + '-' + ('0' + (today.getMonth() + 1)).slice(-2) + '-' + ('0' + today.getDate()).slice(-2);
    }
@@ -31,17 +36,20 @@ export class KardexComponent implements OnInit {
   }
 
   agregarEntrada(){
+    this.kardex.agregar(this.model);
     console.log(this.model);
   }
 
   seleccionarEntrada(){
     this.seleccionadoEntrada = true;
     this.seleccionadoSalida = false;
+    this.model.tipo="entrada";
   }
 
   seleccionarSalida(){
     this.seleccionadoEntrada = false;
     this.seleccionadoSalida = true;
+    this.model.tipo="salida";
   }
 
 }
